8742a11ae06a81a9d91106032691688ee4d363cc
Genilson Junior dos Santos Alves <genilsonjuniordossantos33@gmail.com>
2026-03-24 09:32:34 -0300

    aula sobre leitura de arquivos finalizada (FileReader/BufferedReader)

---
+++ src/Aulas/Aula163/Program.java
@@ -0,0 +1,29 @@
+package Aulas.Aula163;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Program {
+    public static void main(String[] args) {
+
+        String path = "c:\\temp\\in.txt";
+        FileReader fr = null;
+        BufferedReader br = null;
+
+        try {
+            fr = new FileReader(path);
+            br = new BufferedReader(fr);
+            String line = br.readLine();
+
+            while (line != null) {
+                System.out.println(line);
+                line = br.readLine();
+            }
+
+            br.close();
+        } catch (IOException e) {
+            System.out.println("Erro: " + e.getMessage());
+        }
+    }
+}
